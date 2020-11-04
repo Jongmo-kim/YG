941ee718c3eb321c610dcf887c8065d9fa2cb59e
wa1358@naver.com <wa1358@naver.com>
2020-11-04 14:20:37 +0900

    header 완성

---
+++ 03_JS/common.js
@@ -0,0 +1,57 @@
+
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-2.1.4.js';
+script.type ='text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+    $('#nav-icon').on('click', function() {
+            toggleNavIcon();
+            toggleMenuBarDisplayFlex();
+            toggleContentFade();
+            toggleLogoFade();
+        });
+    $(window).on('resize',function(){
+        var windowWidth = $(window).width();
+                    var menubar = $('.menuBar');
+
+        console.log($(menubar).css('display'));
+        if(windowWidth > 719 && $(menubar).css('display')=='flex' ){
+            toggleNavIcon();
+            toggleContentFade();
+            toggleLogoFade();
+            $(menubar).css('display','inline-block');
+        }
+        if(windowWidth < 719 && $(menubar).css('display')=='block'){
+            $(menubar).css('display','none');
+        }
+    });
+        function toggleContentFade(){
+            var content = $('.content');
+            if($(content).css('display')!="none"){
+                $(content).css('display','none');
+            } else{
+                $(content).fadeToggle();
+            }
+        }
+        function toggleLogoFade(){
+            var logo = $('.logo');
+            $(logo).fadeToggle();
+        }
+        function toggleMenuBarDisplayFlex(){
+            var menubar = $('.menuBar');
+            if($(menubar).css('display') == "none"){
+                menubar.css('display','flex');
+            } else {
+                menubar.css('display','none');
+            }
+            
+        }
+        function toggleNavIcon(){
+            var nav = $('#nav-icon');
+            if (!$(nav).hasClass('animate-icon')) {
+                $(nav).addClass('animate-icon');
+            } else {
+                $(nav).removeClass('animate-icon');
+            }
+        }
